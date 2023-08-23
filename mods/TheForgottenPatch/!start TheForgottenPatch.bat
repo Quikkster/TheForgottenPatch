ddd4e32c1ebb3b0125d5e26bc12818f70c062ad7
Quikkster <85338556+Quikkster@users.noreply.github.com>
2023-08-23 12:21:38 -0400

    make Blast Shield worth using

--- mods/TheForgottenPatch/!start TheForgottenPatch.bat
+++ mods/TheForgottenPatch/!start TheForgottenPatch.bat
@@ -5,5 +5,5 @@ cd ..\..\
 set PATH=%localappdata%\xlabs\data\iw4x;%PATH%
 set ServerFilename=FableServersAzza.cfg
 
-start iw4x.exe -nointro +set fs_game "mods\TheForgottenPatch" +exec %ServerFilename% +set g_gametype sd +set ui_gametype sd
+start iw4x.exe -nointro +set fs_game "mods\TheForgottenPatch" +exec %ServerFilename% +set g_gametype war +set ui_gametype war
 @REM start iw4x.exe -console -nointro +set fs_game "mods\TheForgottenPatch" +exec %ServerFilename% +set g_gametype sd +set ui_gametype sd
